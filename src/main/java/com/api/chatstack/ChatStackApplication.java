7c53d1209868b5e32f74692a03a2b7c4d153ec07
ABBASS Mohamad <brt.mabbass@cma-cgm.com>
2025-12-17 09:48:11 +0200

    initial commit

---
+++ src/main/java/com/api/chatstack/ChatStackApplication.java
@@ -0,0 +1,13 @@
+package com.api.chatstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/chatstack/ChatStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.chatstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
