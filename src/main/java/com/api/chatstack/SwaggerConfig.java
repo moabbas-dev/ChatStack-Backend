a243e9d54687f81fe073690632330da334b432cb
ABBASS Mohamad <brt.mabbass@cma-cgm.com>
2025-12-18 10:54:34 +0200

    openapi setup complete

--- src/main/java/com/api/chatstack/SwaggerConfig.java
+++ src/main/java/com/api/chatstack/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,13 +15,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         try {
-            // Read the YAML file content
             String content = new String(Files.readAllBytes(Paths.get(SWAGGER_FILE)));
-
-            // Parse the YAML content into OpenAPI object
             OpenAPIV3Parser parser = new OpenAPIV3Parser();
             return parser.readContents(content, null, null).getOpenAPI();
-
         } catch (IOException e) {
             throw new RuntimeException("Failed to load Swagger YAML file: " + SWAGGER_FILE, e);
         }
